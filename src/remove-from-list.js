672f58395f67a15b5e9e7eb9a1ee6b98ef8c52a0
barkas <vineda.contact@gmail.com>
2023-12-14 14:06:04 +0200

    Revert "feat: implement remove from list"
    This reverts commit f4a46b5b9df6a96963b9fb4c75120412fd989fa4.

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,14 +22,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(l, k) {
-  do {
-    if (l.value === k) {
-      l.value = l.next.value;
-      l.next = l.next.next;
-    }
-  } while (l.next)
-  return l;
+function removeKFromList(/* l, k */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
