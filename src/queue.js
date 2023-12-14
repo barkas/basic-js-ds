6a7db95aa77ad0cea26f99665bc6f506ec4ddf20
barkas <vineda.contact@gmail.com>
2023-12-14 13:07:15 +0200

    feat: implement queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.tail = null;
+    this.head = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    // add to head
+    // const newNode = new ListNode(value);
+    // newNode.next = this.head;
+    // this.head = newNode;
+    // if (!this.tail) {
+    //   this.tail = newNode;
+    // }
+    // add to tail
+    const newNode = new ListNode(value);
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // remove tail
+    // if (!this.tail) {
+    //   return null;
+    // }
+    // const deletedTail = this.tail;
+    // if (this.head === this.tail) {
+    //   this.head = null;
+    //   this.tail = null;
+    //   return deletedTail.value;
+    // }
+  
+    // let currentNode = this.head;
+    // while (currentNode.next) {
+    //   if (!currentNode.next.next) {
+    //     currentNode.next = null;
+    //   } else {
+    //     currentNode = currentNode.next;
+    //   }
+    // }
+    // this.tail = currentNode;  
+    // return deletedTail.value;
+
+    // remove head
+    if (!this.head) {
+      return null;
+    }
+  
+    const deletedHead = this.head;
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+  
+    return deletedHead.value;    
   }
 }
 
